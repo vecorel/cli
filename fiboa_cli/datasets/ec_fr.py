13da06e536b6a775bc8369efe76a2427e6304dce
Matthias Mohr <webmaster@mamo-net.de>
2024-05-06 16:13:24 +0200

    Unpack ZIP, other updates

--- fiboa_cli/datasets/ec_fr.py
+++ fiboa_cli/datasets/ec_fr.py
@@ -1,26 +1,19 @@
-import re
-import pandas as pd
+import os
 
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+
+from ..util import download_file, log
 from ..convert_utils import convert as convert_
 
 # File to read the data from
 # Can read any tabular data format that GeoPandas can read through read_file()
 # Supported protcols: HTTP(S), GCS, S3, or the local file system
-
-# This one doesn't work because the files in the zip are in a folder, not at the root.
-# URI = "https://zenodo.org/records/8229128/files/FR_2018.zip"
-
-# This one doesn't work because the the convert utils doesn't use read_parquet (and most
-# fiona installations won't be built against the parquet library as its not in the wheels).
-# I tried to change the convert utils to use read_parquet, but it didn't work, at least
-# not with this (admittedly large) dataset.
-# URI = "https://data.source.coop/cholmes/eurocrops/geoparquet-projected/FR_2018_EC21.parquet"
-
-# This one should work, but it's a 6 gig file, so it'll take a while to download.
-URI = "https://data.source.coop/cholmes/eurocrops/unprojected/flatgeobuf/FR_2018_EC21.fgb"
+URI = "https://zenodo.org/records/8229128/files/FR_2018.zip"
+FILENAME = "FR_2018/FR_2018_EC21.shp"
 
 # Unique identifier for the collection
-ID = "fr_ec"
+ID = "ec_fr"
 # Title of the collection
 TITLE = "Field boundaries for France from Eurocrops (2018)"
 # Description of the collection. Can be multiline and include CommonMark.
@@ -32,7 +25,7 @@ EuroCrops is a dataset collection combining all publicly available self-declared
 The project is funded by the German Space Agency at DLR on behalf of the Federal Ministry for Economic Affairs and Climate Action (BMWK).
 The work is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License][cc-by-sa].
 
-The project developed a new **Hierarchical Crop and Agriculture Taxonomy (HCAT)** that harmonises all declared crops across the European Union. 
+The project developed a new **Hierarchical Crop and Agriculture Taxonomy (HCAT)** that harmonises all declared crops across the European Union.
 In the data you'll find this as additional attributes:
 
 - `EC_trans_n`: The original crop name translated into English
@@ -40,7 +33,7 @@ In the data you'll find this as additional attributes:
 - `EC_hcat_c`: The 10-digit HCAT code indicating the hierarchy of the crop
 """
 # Bounding box of the data in WGS84 coordinates
-BBOX = [-6.047022416643922, -3.916364769838749, 68.89050422648864, 51.075100624023094] 
+BBOX = [-6.047022416643922, -3.916364769838749, 68.89050422648864, 51.075100624023094]
 
 # Provider name, can be None if not applicable, must be provided if PROVIDER_URL is provided
 PROVIDER_NAME = "Eurocrops"
@@ -50,10 +43,7 @@ PROVIDER_URL = "https://github.com/maja601/EuroCrops/wiki/France"
 ATTRIBUTION = "Institut National de l'Information Géographique et Forestière"
 
 # License of the data, either
-# 1. a SPDX license identifier (including "dl-de/by-2-0" / "dl-de/zero-2-0"), or
 LICENSE = "CC-BY-2.0"
-# 2. a STAC Link Object with relation type "license"
-# LICENSE = {'title': 'CC-BY-4.0', 'href': 'https://creativecommons.org/licenses/by/4.0/', 'type': 'text/html', 'rel': 'license'}
 
 # Map original column names to fiboa property names
 COLUMNS = {
@@ -85,8 +75,7 @@ ADD_COLUMNS = {
 # Filter columns to only include the ones that are relevant for the collection,
 # e.g. only rows that contain the word "agriculture" but not "forest" in the column "land_cover_type".
 # Lamda function accepts a Pandas Series and returns a Series or a Tuple with a Series and True to inverse the mask.
-COLUMN_FILTERS = {
-}
+COLUMN_FILTERS = {}
 
 # Custom function to migrate the GeoDataFrame if the other options are not sufficient
 # This should be the last resort!
@@ -120,51 +109,37 @@ MISSING_SCHEMAS = {
 
 # Conversion function, usually no changes required
 def convert(output_file, cache_file = None, source_coop_url = None, collection = False, compression = None):
-    """
-    Converts the field boundary datasets to fiboa.
+    # We need to extract manually because the GeoPackage is zipped and in a folder, not at the root
+    log("Loading file from: " + URI)
+    path = download_file(URI, cache_file)
 
-    For reference, this is the order in which the conversion steps are applied:
-    0. Read GeoDataFrame from file
-    1. Run global migration (if provided through MIGRATION)
-    2. Run filters to remove rows that shall not be in the final data
-       (if provided through COLUMN_FILTERS)
-    3. Run column migrations (if provided through COLUMN_MIGRATIONS)
-    4. Duplicate columns (if an array is provided as the value in COLUMNS)
-    5. Rename columns (as provided in COLUMNS)
-    6. Remove columns (if column is not present as value in COLUMNS)
-    7. Create the collection
-    8. Change data types of the columns based on the provided schemas
-    (fiboa spec, extensions, and MISSING_SCHEMAS)
-    9. Write the data to the Parquet file
+    with TemporaryDirectory() as tmp_dir:
+        log("Unzipping to: " + tmp_dir)
+        with ZipFile(path, 'r') as f:
+            f.extractall(tmp_dir)
 
-    Parameters:
-    output_file (str): Path where the Parquet file shall be stored.
-    cache_file (str): Path to a cached file of the data. Default: None.
-                      Can be used to avoid repetitive downloads from the original data source.
-    source_coop_url (str): URL to the (future) Source Cooperative repository. Default: None
-    collection (bool): Additionally, store the collection separate from Parquet file. Default: False
-    kwargs: Additional keyword arguments for GeoPandas read_file() function.
-    """
-    convert_(
-        output_file,
-        cache_file,
-        URI,
-        COLUMNS,
-        ID,
-        TITLE,
-        DESCRIPTION,
-        BBOX,
-        provider_name=PROVIDER_NAME,
-        provider_url=PROVIDER_URL,
-        source_coop_url=source_coop_url,
-        extensions=EXTENSIONS,
-        missing_schemas=MISSING_SCHEMAS,
-        column_migrations=COLUMN_MIGRATIONS,
-        column_filters=COLUMN_FILTERS,
-        column_additions=ADD_COLUMNS,
-        migration=MIGRATION,
-        attribution=ATTRIBUTION,
-        store_collection=collection,
-        license=LICENSE,
-        compression=compression
-    )
+        new_path = os.path.join(tmp_dir, FILENAME)
+
+        convert_(
+            output_file,
+            cache_file,
+            new_path,
+            COLUMNS,
+            ID,
+            TITLE,
+            DESCRIPTION,
+            BBOX,
+            provider_name=PROVIDER_NAME,
+            provider_url=PROVIDER_URL,
+            source_coop_url=source_coop_url,
+            extensions=EXTENSIONS,
+            missing_schemas=MISSING_SCHEMAS,
+            column_migrations=COLUMN_MIGRATIONS,
+            column_filters=COLUMN_FILTERS,
+            column_additions=ADD_COLUMNS,
+            migration=MIGRATION,
+            attribution=ATTRIBUTION,
+            store_collection=collection,
+            license=LICENSE,
+            compression=compression,
+        )
