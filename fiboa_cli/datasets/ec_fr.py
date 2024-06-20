355a9cfe8144a649b98346b452ac543baef712e1
Matthias Mohr <webmaster@mamo-net.de>
2024-06-20 13:47:58 +0200

    Clean-up EuroCrops templates

--- fiboa_cli/datasets/ec_fr.py
+++ fiboa_cli/datasets/ec_fr.py
@@ -1,17 +1,12 @@
 from ..convert_utils import convert as convert_
 
-# File to read the data from
 SOURCES = {
   "https://zenodo.org/records/10118572/files/FR_2018.zip": ["FR_2018/FR_2018_EC21.shp"]
 }
 
-# Unique identifier for the collection
 ID = "ec_fr"
-# Geonames for the data
 SHORT_NAME = "France (Eurocrops, 2018)"
-# Title of the collection
 TITLE = "Field boundaries for France from EuroCrops (2018)"
-# Description of the collection. Can be multiline and include CommonMark.
 DESCRIPTION = """
 This dataset contains the field boundaries for all of France in 2018. The data was collected by the French government and harmonized
 by the EuroCrops project and is available on Zenodo.
@@ -28,17 +23,12 @@ In the data you'll find this as additional attributes:
 - `EC_hcat_c`: The 10-digit HCAT code indicating the hierarchy of the crop
 """
 
-# Provider name, can be None if not applicable, must be provided if PROVIDER_URL is provided
 PROVIDER_NAME = "EuroCrops"
-# URL to the homepage of the data or the provider, can be None if not applicable
 PROVIDER_URL = "https://github.com/maja601/EuroCrops/wiki/France"
-# Attribution, can be None if not applicable
 ATTRIBUTION = "Institut National de l'Information Géographique et Forestière"
 
-# License of the data, either
 LICENSE = "CC-BY-4.0"
 
-# Map original column names to fiboa property names
 COLUMNS = {
     'geometry': 'geometry', #fiboa core field
     'ID_PARCEL': 'id', #fiboa core field
@@ -50,36 +40,12 @@ COLUMNS = {
     'EC_hcat_c': 'EC_hcat_c' #fiboa custom field
 }
 
-# A list of implemented extension identifiers
-EXTENSIONS = []
-
-# Functions to migrate data in columns to match the fiboa specification.
-# Example: You have a column area_m in square meters and want to convert
-# to hectares as required for the area field in fiboa.
-# Function signature:
-#   func(column: pd.Series) -> pd.Series
-COLUMN_MIGRATIONS = {}
-
 ADD_COLUMNS = {
   "determination_datetime": "2018-01-15T00:00:00Z"
 }
 
-
-# Filter columns to only include the ones that are relevant for the collection,
-# e.g. only rows that contain the word "agriculture" but not "forest" in the column "land_cover_type".
-# Lamda function accepts a Pandas Series and returns a Series or a Tuple with a Series and True to inverse the mask.
-COLUMN_FILTERS = {}
-
-# Custom function to migrate the GeoDataFrame if the other options are not sufficient
-# This should be the last resort!
-# Function signature:
-#   func(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame
-MIGRATION = None
-
-# Schemas for the fields that are not defined in fiboa
-# Keys must be the values from the COLUMNS dict, not the keys
 MISSING_SCHEMAS = {
-    'required': ['code_culture', 'code_group', 'EC_trans_n','EC_hcat_n', 'EC_hcat_c'], # i.e. non-nullable properties
+    'required': ['code_culture', 'code_group', 'EC_trans_n','EC_hcat_n', 'EC_hcat_c'],
     'properties': {
         'code_culture': {
             'type': 'string'
@@ -100,7 +66,6 @@ MISSING_SCHEMAS = {
 }
 
 
-# Conversion function, usually no changes required
 def convert(output_file, cache = None, source_coop_url = None, collection = False, compression = None):
     convert_(
         output_file,
@@ -113,12 +78,8 @@ def convert(output_file, cache = None, source_coop_url = None, collection = Fals
         provider_name=PROVIDER_NAME,
         provider_url=PROVIDER_URL,
         source_coop_url=source_coop_url,
-        extensions=EXTENSIONS,
         missing_schemas=MISSING_SCHEMAS,
-        column_migrations=COLUMN_MIGRATIONS,
-        column_filters=COLUMN_FILTERS,
         column_additions=ADD_COLUMNS,
-        migration=MIGRATION,
         attribution=ATTRIBUTION,
         store_collection=collection,
         license=LICENSE,
--- fiboa_cli/datasets/ec_si.py
+++ fiboa_cli/datasets/ec_si.py
@@ -1,17 +1,12 @@
 from ..convert_utils import convert as convert_
 
-# File to read the data from
 SOURCES = {
   "https://zenodo.org/records/10118572/files/SI_2021.zip?download=1": ["SI_2021_EC21.shp"]
 }
 
-# Unique identifier for the collection
 ID = "ec_si"
-# Geonames for the data
 SHORT_NAME = "Slovenia (Eurocrops, 2021)"
-# Title of the collection
 TITLE = "Field boundaries for Slovenia from EuroCrops (2021)"
-# Description of the collection. Can be multiline and include CommonMark.
 DESCRIPTION = """
 This dataset contains the field boundaries for all of Slovenia in 2021. The data was collected by the Slovenian government and harmonized
 by the EuroCrops project and is available on Zenodo.
@@ -28,21 +23,12 @@ In the data you'll find this as additional attributes:
 - `EC_hcat_c`: The 10-digit HCAT code indicating the hierarchy of the crop
 """
 
-# Provider name, can be None if not applicable, must be provided if PROVIDER_URL is provided
 PROVIDER_NAME = "EuroCrops"
-# URL to the homepage of the data or the provider, can be None if not applicable
 PROVIDER_URL = "https://github.com/maja601/EuroCrops/wiki/Slovenia"
-# Attribution, can be None if not applicable
 ATTRIBUTION = "Ministrstvo za kmetijstvo, gozdarstvo in prehrano"
 
-# License of the data, either
-# 1. a SPDX license identifier (including "dl-de/by-2-0" / "dl-de/zero-2-0"), or
 LICENSE = "CC-BY-4.0"
-# 2. a STAC Link Object with relation type "license"
-# LICENSE = {"title": "CC-BY-4.0", "href": "https://creativecommons.org/licenses/by/4.0/", "type": "text/html", "rel": "license"}
 
-# Map original column names to fiboa property names
-# You also need to list any column that you may have added in the MIGRATION function (see below).
 COLUMNS = {
     'geometry': 'geometry', #fiboa core field
     'ID': 'id', #fiboa core field
@@ -57,37 +43,12 @@ COLUMNS = {
     'EC_hcat_c': 'EC_hcat_c' #fiboa custom field
 }
 
-# Add columns with constant values.
-# The key is the column name, the value is a constant value that's used for all rows.
 ADD_COLUMNS = {
     "determination_datetime": "2021-10-06T00:00:00Z"
 }
 
-# A list of implemented extension identifiers
-EXTENSIONS = []
-
-# Functions to migrate data in columns to match the fiboa specification.
-# Example: You have a column area_m in square meters and want to convert
-# to hectares as required for the area field in fiboa.
-# Function signature:
-#   func(column: pd.Series) -> pd.Series
-COLUMN_MIGRATIONS = {}
-
-# Filter columns to only include the ones that are relevant for the collection,
-# e.g. only rows that contain the word "agriculture" but not "forest" in the column "land_cover_type".
-# Lamda function accepts a Pandas Series and returns a Series or a Tuple with a Series and True to inverse the mask.
-COLUMN_FILTERS = {}
-
-# Custom function to migrate the GeoDataFrame if the other options are not sufficient
-# This should be the last resort!
-# Function signature:
-#   func(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame
-MIGRATION = None
-
-# Schemas for the fields that are not defined in fiboa
-# Keys must be the values from the COLUMNS dict, not the keys
 MISSING_SCHEMAS = {
-    'required': ['gerk_pid', 'crop_type_class', 'rastlina', 'crop_latin', 'color', 'EC_trans_n','EC_hcat_n'], # i.e. non-nullable properties
+    'required': ['gerk_pid', 'crop_type_class', 'rastlina', 'crop_latin', 'color', 'EC_trans_n','EC_hcat_n'],
     'properties': {
         'gerk_pid': {
             'type': 'uint64'
@@ -117,35 +78,7 @@ MISSING_SCHEMAS = {
 }
 
 
-# Conversion function, usually no changes required
 def convert(output_file, cache = None, source_coop_url = None, collection = False, compression = None):
-    """
-    Converts the field boundary datasets to fiboa.
-
-    For reference, this is the order in which the conversion steps are applied:
-    0. Read GeoDataFrame from file
-    1. Run global migration (if provided through MIGRATION)
-    2. Run filters to remove rows that shall not be in the final data
-       (if provided through COLUMN_FILTERS)
-    3. Add columns with constant values
-    4. Run column migrations (if provided through COLUMN_MIGRATIONS)
-    5. Duplicate columns (if an array is provided as the value in COLUMNS)
-    6. Rename columns (as provided in COLUMNS)
-    7. Remove columns (if column is not present as value in COLUMNS)
-    8. Create the collection
-    9. Change data types of the columns based on the provided schemas
-    (fiboa spec, extensions, and MISSING_SCHEMAS)
-    10. Write the data to the Parquet file
-
-    Parameters:
-    output_file (str): Path where the Parquet file shall be stored.
-    cache (str): Path to a cached file of the data. Default: None.
-                      Can be used to avoid repetitive downloads from the original data source.
-    source_coop_url (str): URL to the (future) Source Cooperative repository. Default: None
-    collection (bool): Additionally, store the collection separate from Parquet file. Default: False
-    compression (str): Compression method for the Parquet file. Default: zstd
-    kwargs: Additional keyword arguments for GeoPanda's read_file() or read_parquet() function.
-    """
     convert_(
         output_file,
         cache,
@@ -157,12 +90,8 @@ def convert(output_file, cache = None, source_coop_url = None, collection = Fals
         provider_name=PROVIDER_NAME,
         provider_url=PROVIDER_URL,
         source_coop_url=source_coop_url,
-        extensions=EXTENSIONS,
         missing_schemas=MISSING_SCHEMAS,
-        column_migrations=COLUMN_MIGRATIONS,
-        column_filters=COLUMN_FILTERS,
         column_additions=ADD_COLUMNS,
-        migration=MIGRATION,
         attribution=ATTRIBUTION,
         store_collection=collection,
         license=LICENSE,
