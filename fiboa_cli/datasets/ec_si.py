c8d3572ff1bf8bbef2ccc57e0bc9bcc9d2f01c80
Daniel Nwaeze <57524983+DanielNwaeze@users.noreply.github.com>
2024-06-23 16:23:59 +0100

    Update ec_si.py (#71)

--- fiboa_cli/datasets/ec_si.py
+++ fiboa_cli/datasets/ec_si.py
@@ -21,6 +21,11 @@ In the data you'll find this as additional attributes:
 - `EC_trans_n`: The original crop name translated into English
 - `EC_hcat_n`: The machine-readable HCAT name of the crop
 - `EC_hcat_c`: The 10-digit HCAT code indicating the hierarchy of the crop
+- `EC_nuts3`: The Nomenclature of Territorial Units for Statistics 3 (NUTS3) region, an approximate assignment of a crop parcel to a region
+
+Disclaimer: The Nomenclature of Territorial Units for Statistics 3 (NUTS3) region, which we added by hand, is just an approximate assignment of a
+crop parcel to a region. It might happen that a parcel is not correctly allocated to the right region or country. The NUTS3 attribute is only meant
+to be an aid for a meaningful spatial division of the dataset into training, validation, and test sets.
 """
 
 PROVIDER_NAME = "EuroCrops"
@@ -40,15 +45,14 @@ COLUMNS = {
     'COLOR': 'color', #fiboa custom field
     'EC_trans_n': 'EC_trans_n', #fiboa custom field
     'EC_hcat_n': 'EC_hcat_n', #fiboa custom field
-    'EC_hcat_c': 'EC_hcat_c' #fiboa custom field
+    'EC_hcat_c': 'EC_hcat_c', #fiboa custom field
+    'EC_NUTS3': 'EC_NUTS3', #fiboa custom field
 }
 
-ADD_COLUMNS = {
-    "determination_datetime": "2021-10-06T00:00:00Z"
-}
+ADD_COLUMNS = {}
 
 MISSING_SCHEMAS = {
-    'required': ['gerk_pid', 'crop_type_class', 'rastlina', 'crop_latin', 'color', 'EC_trans_n','EC_hcat_n'],
+    'required': ['gerk_pid', 'crop_type_class', 'rastlina', 'crop_lat_e', 'color', 'EC_trans_n','EC_hcat_n'],
     'properties': {
         'gerk_pid': {
             'type': 'uint64'
@@ -73,6 +77,9 @@ MISSING_SCHEMAS = {
         },
         'EC_hcat_c': {
             'type': 'uint32'
+        },
+        'EC_NUTS3': {
+            'type': 'string'
         }
     }
 }
