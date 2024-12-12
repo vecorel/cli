6fd6288520101c57d1946e000b92ba298a408762
Ivor Bosloper <ivorbosloper@gmail.com>
2024-12-12 12:56:45 +0100

    Extend for multiple years, test everything

--- fiboa_cli/datasets/ie.py
+++ fiboa_cli/datasets/ie.py
@@ -1,71 +1,54 @@
 from ..convert_gml import gml_assure_columns
-from ..convert_utils import convert as convert_
+from ..convert_utils import convert as convert_, BaseConverter
 
-SOURCES = {
-  "https://osi-inspire-atom.s3-eu-west-1.amazonaws.com/IACSdata/IACS_GSAA_2022.zip": ["IACS_GSAA_2022.gml"]
-}
-LAYER = "ExistingLandUseObject"
 
-ID = "ie"
-SHORT_NAME = "Ireland"
-TITLE = "Ireland INSPIRE Geospatial aid application (GSAA) dataset"
-DESCRIPTION = """
-This data represents the outline shape of LPIS parcels as claimed under area based schemes. The dataset includes the crops claimed as part of the annual GSAA.
-Yearly information provided through the beneficiary declaration.
-"""
-
-PROVIDERS = [
-    {
-        "name": "Ireland Department of Agriculture, Food and the Marine",
-        "url": "https://inspire.geohive.ie/geoportal/",
-        "roles": ['producer', 'licensor']
+class IEConverter(BaseConverter):
+    sources = {
+      "https://osi-inspire-atom.s3-eu-west-1.amazonaws.com/IACSdata/IACS_GSAA_2022.zip": ["IACS_GSAA_2022.gml"]
     }
-]
-ATTRIBUTION = "Ireland Department of Agriculture, Food and the Marine"
-LICENSE = "CC-BY-4.0"
-COLUMNS = {
-    "geometry": "geometry",
-    "crop_name": "crop_name",
-    "localId": "id",
-    "determination_datetime": "determination_datetime",
-}
 
-MISSING_SCHEMAS = {
-    "properties": {
-        "crop_name": {
-            "type": "string"
-        },
+    id = "ie"
+    short_name = "Ireland"
+    title = "Ireland INSPIRE Geospatial aid application (GSAA) dataset"
+    description = """
+    This data represents the outline shape of LPIS parcels as claimed under area based schemes. The dataset includes the crops claimed as part of the annual GSAA.
+    Yearly information provided through the beneficiary declaration.
+    """
+
+    providers = [
+        {
+            "name": "Ireland Department of Agriculture, Food and the Marine",
+            "url": "https://inspire.geohive.ie/geoportal/",
+            "roles": ['producer', 'licensor']
+        }
+    ]
+    attribution = "Ireland Department of Agriculture, Food and the Marine"
+    license = "CC-BY-4.0"
+    columns = {
+        "geometry": "geometry",
+        "crop_name": "crop_name",
+        "localId": "id",
+        "determination_datetime": "determination_datetime",
     }
-}
 
+    missing_schemas = {
+        "properties": {
+            "crop_name": {
+                "type": "string"
+            },
+        }
+    }
 
-def migration(gdf):
-    # crop_name can be multiple: "crop1, crop2, crop3". We only read the main crop (first).
-    gdf['crop_name'] = gdf['crop_name'].str.split(', ').str.get(0)
-    gdf = gdf[gdf['crop_name'] != 'Void']  # Exclude non-agriculture fields
-    gdf["determination_datetime"] = gdf["observationDate"].str.replace("+01:00", "T00:00:00Z")
-    return gdf
+    def migrate(self, gdf):
+        # crop_name can be multiple: "crop1, crop2, crop3". We only read the main crop (first).
+        gdf['crop_name'] = gdf['crop_name'].str.split(', ').str.get(0)
+        gdf = gdf[gdf['crop_name'] != 'Void']  # Exclude non-agriculture fields
+        gdf["determination_datetime"] = gdf["observationDate"].str.replace("+01:00", "T00:00:00Z")
+        return gdf
 
-
-def convert(output_file, cache = None, **kwargs):
-    def file_migration(data, path, uri, layer):
+    def file_migration(self, data, path, uri, layer):
         return gml_assure_columns(data, path, uri, layer,
                                   crop_name={"ElementPath": "specificLandUse@title", "Type": "String", "Width": 255})
 
-    convert_(
-        output_file,
-        cache,
-        SOURCES,
-        COLUMNS,
-        ID,
-        TITLE,
-        DESCRIPTION,
-        providers=PROVIDERS,
-        missing_schemas=MISSING_SCHEMAS,
-        migration=migration,
-        attribution=ATTRIBUTION,
-        license=LICENSE,
-        layer_filter=lambda layer, uri: layer == LAYER,
-        file_migration=file_migration,
-        **kwargs
-    )
+    def layer_filter(self, layer, uri):
+        return layer == "ExistingLandUseObject"
--- fiboa_cli/datasets/jp.py
+++ fiboa_cli/datasets/jp.py
@@ -1,77 +1,62 @@
-from ..convert_utils import convert as convert_
+from ..convert_utils import convert as convert_, BaseConverter
 import pandas as pd
 
-YEARS = {
-    2021: "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2021.parquet",
-    2022: "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2022.parquet",
-    2023: "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2023.parquet",
-    2024: "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2024.parquet",
-}
 
-ID = "jp"
-SHORT_NAME = "Japan"
-TITLE = "Japan Fude Parcels"
-DESCRIPTION = """
-Japanese Farmland Parcel Polygons (Fude Polygons in Japanese) represent parcel information of farmland.
-The polygons are manually digitized data derived from aerial imagery, such as satellite images. Since no
-on-site verification or similar procedures have been conducted, the data may not necessarily match the actual
-current conditions. Fude Polygons are created for the purpose of roughly indicating the locations of farmland.
-"""
-
-PROVIDERS = [
-    {
-        "name": "Japanese Ministry of Agriculture, Forestry and Fisheries (MAFF, 農林水産省)",
-        "url": "https://www.maff.go.jp/",
-        "roles": ["licensor", "producer"]
-    },
-    {
-        "name": "Hiroo Imaki, Pacific Spatial Solutions",
-        "url": "https://pacificspatial.com/",
-        "roles": ["processor"],
+class JPConverter(BaseConverter):
+    source_variants = {
+        "2024": "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2024.parquet",
+        "2023": "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2023.parquet",
+        "2022": "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2022.parquet",
+        "2021": "https://data.source.coop/pacificspatial/field-polygon-jp/parquet/jp_field_polygons_2021.parquet",
     }
-]
-ATTRIBUTION = "Fude Polygon Data (2021-2024). Japanese Ministry of Agriculture, Forestry and Fisheries. Processed by Pacific Spatial Solutions, Inc"
-LICENSE = "CC-BY-4.0"
 
-COLUMNS = {
-    "GEOM": "geometry",
-    'polygon_uuid': 'id',
-    'land_type_en': "land_type_en",
-    'local_government_cd': 'admin_local_code',
-    "issue_year": "determination_datetime",
-}
-COLUMN_MIGRATIONS = {
-    "issue_year": lambda col: pd.to_datetime(col, format='%Y'),
-}
+    id = "jp"
+    short_name = "Japan"
+    title = "Japan Fude Parcels"
+    description = """
+    Japanese Farmland Parcel Polygons (Fude Polygons in Japanese) represent parcel information of farmland.
+    The polygons are manually digitized data derived from aerial imagery, such as satellite images. Since no
+    on-site verification or similar procedures have been conducted, the data may not necessarily match the actual
+    current conditions. Fude Polygons are created for the purpose of roughly indicating the locations of farmland.
+    """
 
-MISSING_SCHEMAS = {
-    'properties': {
-        'land_type_en': {
-            'type': 'string'
+    providers = [
+        {
+            "name": "Japanese Ministry of Agriculture, Forestry and Fisheries (MAFF, 農林水産省)",
+            "url": "https://www.maff.go.jp/",
+            "roles": ["licensor", "producer"]
         },
-        'admin_local_code': {
-            'type': 'string'
-        },
+        {
+            "name": "Hiroo Imaki, Pacific Spatial Solutions",
+            "url": "https://pacificspatial.com/",
+            "roles": ["processor"],
+        }
+    ]
+    attribution = "Fude Polygon Data (2021-2024). Japanese Ministry of Agriculture, Forestry and Fisheries. Processed by Pacific Spatial Solutions, Inc"
+    license = "CC-BY-4.0"
+
+    columns = {
+        "GEOM": "geometry",
+        'polygon_uuid': 'id',
+        'land_type_en': "land_type_en",
+        'local_government_cd': 'admin_local_code',
+        "issue_year": "determination_datetime",
     }
-}
+    column_migrations = {
+        "issue_year": lambda col: pd.to_datetime(col, format='%Y'),
+    }
 
+    missing_schemas = {
+        'properties': {
+            'land_type_en': {
+                'type': 'string'
+            },
+            'admin_local_code': {
+                'type': 'string'
+            },
+        }
+    }
 
-def convert(output_file, cache = None, **kwargs):
-    year = 2024  # TODO make parameter
-    sources = {YEARS[year]: f"jp_field_polygons_{year}.parquet"}
-    convert_(
-        output_file,
-        cache,
-        sources,
-        COLUMNS,
-        ID,
-        TITLE,
-        DESCRIPTION,
-        missing_schemas=MISSING_SCHEMAS,
-        column_migrations=COLUMN_MIGRATIONS,
-        providers=PROVIDERS,
-        attribution=ATTRIBUTION,
-        license=LICENSE,
-        columns=["GEOM", "polygon_uuid", "land_type_en", "local_government_cd", "issue_year"],
-        **kwargs
-    )
+    def convert(self, *args, **kwargs):
+        # Open only these columns to limit memory usage
+        super().convert(*args, columns=["GEOM", "polygon_uuid", "land_type_en", "local_government_cd", "issue_year"], **kwargs)
