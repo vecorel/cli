3601fae20d3f39abb0dc71b741e9c8d43b6d09a8
Ivor Bosloper <ivorbosloper@gmail.com>
2025-02-03 11:53:03 +0100

    Sort geometries by hilbert_distance

--- fiboa_cli/convert_utils.py
+++ fiboa_cli/convert_utils.py
@@ -521,6 +521,7 @@ class BaseConverter:
         log("GeoDataFrame fully migrated:")
         print(gdf.head())
 
+        gdf.sort_values('geometry', inplace=True, ignore_index=True)
         collection = self.create_collection(gdf, source_coop_url=source_coop_url)
 
         log("Creating GeoParquet file: " + output_file)
