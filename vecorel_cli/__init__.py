5403aaff3b96bcbff5e8b3809813884abcf3cff6
Matthias Mohr <webmaster@mamo-net.de>
2025-08-25 12:20:21 +0200

    Restructure the CLI for avoiding the import race condition

--- vecorel_cli/__init__.py
+++ vecorel_cli/__init__.py
@@ -1,22 +1,15 @@
-import click
-
 from .registry import Registry
 
+if __name__ == Registry.src_package or __name__ == "__main__":
+    import click
 
-@click.group()
-@click.version_option(version=Registry.get_version(), prog_name=Registry.cli_title)
-def vecorel_cli():
-    pass
-
-
-commands = Registry.get_commands()
-for command in commands:
-    click_cmd = command.get_cli_command(command)
-    click_args = command.get_cli_args().values()
-    for arg in click_args:
-        click_cmd = arg(click_cmd)
-    vecorel_cli.add_command(click_cmd)
-
-
-if __name__ == "__main__":
-    vecorel_cli()
+    @click.group()
+    @click.version_option(version=Registry.get_version(), prog_name=Registry.cli_title)
+    def vecorel_cli():
+        commands = Registry.get_commands()
+        for command in commands:
+            click_cmd = command.get_cli_command(command)
+            click_args = command.get_cli_args().values()
+            for arg in click_args:
+                click_cmd = arg(click_cmd)
+            vecorel_cli.add_command(click_cmd)
